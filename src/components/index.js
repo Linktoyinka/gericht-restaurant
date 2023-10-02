feedeff1ee5588689cedd3fdf943d3098625ae53
Afolabi <linktoyinka@gmail.com>
2023-10-02 15:55:11 +0100

    menu section added

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar'
-export { default as Menus } from './Menus/Menus'
+export { default as MenuItem } from './Menus/MenuItem'
 export { default as Footer } from './Footer/Footer'
 export { default as SubHeading } from './SubHeading/SubHeading'
